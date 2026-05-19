003cfaa0683dffb5d47d7d2580e52a37dd168d58
Denchik <rock333shadow@gmail.com>
2026-05-19 22:25:22 +0300

    Added QuestProgressService to providers

--- lib/core/providers/providers.dart
+++ lib/core/providers/providers.dart
@@ -4,14 +4,24 @@ import 'package:provider/provider.dart';
 
 import 'package:zentea/core/l10n/achievement_localization.dart';
 
+import 'package:zentea/services/achievements/i_achievements_service.dart';
 import 'package:zentea/services/achievements/achievements_service.dart';
+
+import 'package:zentea/services/quest_progress/quest_progress_service.dart';
+
 import 'package:zentea/services/settings/settings_service.dart';
-import 'package:zentea/services/storage/hive_key_value_storage.dart';
-import 'package:zentea/services/tea_collection/tea_collection_service.dart';
-import 'package:zentea/services/today_tea/today_tea_service.dart';
-import 'package:zentea/services/today_tea/today_tea_service_impl.dart';
+
+import 'package:zentea/services/stats/i_stats_service.dart';
 import 'package:zentea/services/stats/stats_provider.dart';
 import 'package:zentea/services/stats/hive_stats_service.dart';
+
+import 'package:zentea/services/storage/i_key_value_storage.dart';
+import 'package:zentea/services/storage/hive_key_value_storage.dart';
+
+import 'package:zentea/services/tea_collection/tea_collection_service.dart';
+
+import 'package:zentea/services/today_tea/today_tea_service.dart';
+import 'package:zentea/services/today_tea/today_tea_service_impl.dart';
 
 List<SingleChildWidget> buildProviders() => [
   Provider(
@@ -39,6 +49,14 @@ List<SingleChildWidget> buildProviders() => [
   ChangeNotifierProvider(
     create: (_) => TeaCollectionService(
       HiveKeyValueStorage(Hive.box('tea_collection')),
+    ),
+  ),
+
+  ChangeNotifierProvider(
+    create: (ctx) => QuestProgressService(
+      statsService: ctx.read<IStatsService>(),
+      storage: ctx.read<IKeyValueStorage>(),
+      achievementsService: ctx.read<IAchievementsService>(),
     ),
   ),
 
