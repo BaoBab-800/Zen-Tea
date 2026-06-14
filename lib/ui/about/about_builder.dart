ba5be467531971c91d7b049bda288d1b5308709a
Denchik <rock333shadow@gmail.com>
2026-06-14 13:25:54 +0300

    Content of the about page has been moved to a new file.

--- lib/ui/about/about_builder.dart
+++ lib/ui/about/about_builder.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:zentea/core/l10n/l10n.dart';
-import 'package:zentea/core/theme/app_theme.dart';
+
+import 'about_content.dart';
 
 class AboutBuilder extends StatelessWidget {
   const AboutBuilder({super.key});
@@ -16,84 +17,7 @@ class AboutBuilder extends StatelessWidget {
         ),
       ),
 
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-          margin: const EdgeInsets.all(16),
-
-          decoration: BoxDecoration(
-            color: context.colors.surface,
-            borderRadius: BorderRadius.circular(16.0),
-          ),
-
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  Text(
-                    'ZenTea',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w400,
-                      fontStyle: FontStyle.italic,
-                      letterSpacing: 1.5,
-                    ),
-                  ),
-
-                  SizedBox(height: 6),
-                  Container(
-                    width: 60,
-                    height: 1,
-                    color: context.colors.primary,
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 14.0),
-              Text(
-                context.l10n.aboutApplicationDescription,
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-              ),
-
-              const SizedBox(height: 12.0),
-              Text(
-                context.l10n.aboutWhyDoYouNeedThisApp,
-                textAlign: TextAlign.center,
-              ),
-
-              const SizedBox(height: 18.0),
-              Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: context.colors.primary.withValues(alpha: 0.2),
-                  borderRadius: BorderRadius.circular(16),
-                ),
-
-                child: Text(
-                  context.l10n.aboutPhilosophy,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontStyle: FontStyle.italic,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 18.0),
-              Text(context.l10n.aboutTeam),
-
-              const SizedBox(height: 12.0),
-              Text(
-                context.l10n.aboutVersion,
-                style: TextStyle(color: Colors.grey),
-              ),
-            ],
-          ),
-        ),
-      ),
+      body: AboutContent(),
     );
   }
 }
---
+++ lib/ui/about/about_content.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+import 'package:zentea/core/theme/app_theme.dart';
+import 'package:zentea/core/l10n/l10n.dart';
+
+class AboutContent extends StatelessWidget {
+  const AboutContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+        margin: const EdgeInsets.all(16),
+
+        decoration: BoxDecoration(
+          color: context.colors.surface,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Text(
+                  'ZenTea',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.italic,
+                    letterSpacing: 1.5,
+                  ),
+                ),
+
+                SizedBox(height: 6),
+                Container(
+                  width: 60,
+                  height: 1,
+                  color: context.colors.primary,
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 14.0),
+            Text(
+              context.l10n.aboutApplicationDescription,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+
+            const SizedBox(height: 12.0),
+            Text(
+              context.l10n.aboutWhyDoYouNeedThisApp,
+              textAlign: TextAlign.center,
+            ),
+
+            const SizedBox(height: 18.0),
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: context.colors.primary.withValues(alpha: 0.2),
+                borderRadius: BorderRadius.circular(16),
+              ),
+
+              child: Text(
+                context.l10n.aboutPhilosophy,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 18.0),
+            Text(context.l10n.aboutTeam),
+
+            const SizedBox(height: 12.0),
+            Text(
+              context.l10n.aboutVersion,
+              style: TextStyle(color: Colors.grey),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
