8b24a99cae1be37790b8b2b789bd9995505d07e5
Denchik <rock333shadow@gmail.com>
2026-06-15 20:36:49 +0300

    Quotes have been added to His page. (They are currently empty)

---
+++ lib/data/his/his_quotes.dart
@@ -0,0 +1,13 @@
+List<String> hisQuotes = [
+  'Quote 0',
+  'Quote 1',
+  'Quote 2',
+  'Quote 3',
+  'Quote 4',
+  'Quote 5',
+  'Quote 6',
+  'Quote 7',
+];
+
+// 'He had no fear.',
+// 'He sees what most people don\'t see.',
--- lib/ui/his_page/his_page_content.dart
+++ lib/ui/his_page/his_page_content.dart
@@ -1,13 +1,60 @@
 import 'package:flutter/material.dart';
 
+import 'package:zentea/data/his/his_quotes.dart';
+
 class HisPageContent extends StatelessWidget {
   const HisPageContent({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('His Page'),
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: hisQuotes.length,
+        itemBuilder: (context, index) {
+          return HisQuoteCard(quote: hisQuotes[index], index: index);
+        },
+      ),
+    );
+  }
+}
+
+class HisQuoteCard extends StatelessWidget {
+  final String quote;
+  final int index;
+
+  const HisQuoteCard({
+    super.key,
+    required this.quote,
+    required this.index,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final archiveId = (index + 1).toString().padLeft(3, '0');
+
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'ARCHIVE_$archiveId',
+            style: const TextStyle(
+              fontSize: 12,
+              letterSpacing: 2,
+            ),
+          ),
+
+          const SizedBox(height: 8),
+
+          Text(
+            quote,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
       ),
     );
   }
