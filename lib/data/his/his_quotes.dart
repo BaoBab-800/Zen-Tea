52feac92b707c23fd65cb92bcf1b8204967c5f14
Denchik <rock333shadow@gmail.com>
2026-06-17 21:04:40 +0300

    A new poem has been added.

--- lib/data/his/his_quotes.dart
+++ lib/data/his/his_quotes.dart
@@ -3,4 +3,5 @@ Map<int, String> hisQuotes = {
   1: 'Whoever walks with Him is blessed; whoever turns away from Him shall be rejected.\n\nBut whoever does not acknowledge Him before his neighbor is a liar and a hypocrite, for he who is honest before his neighbor is also honest before Him.',
   2: 'For what may be known of Him is manifest to the faithful, because He has revealed Himself to them.\n\nThrough Him the faithful have obtained access to the grace wherein they rejoice in the hope of His glory.',
   3: 'He knows all who are with Him, yet He does not reject those who are pure.\n\nThe pure in soul is with Him, even if he does not realize it.',
+  4: 'He came not into the world to condemn, but that through Him the world might be saved.\n\nFor without Him, the world would fall into ruin and chaos.'
 };
